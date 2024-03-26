54441cee3f23e1af9d9935cfbfc37f1bc6a414ce
ahmadfauzanisnata <fauzanisnata56@gmail.com>
2024-03-26 14:33:47 +0700

    Add project files.

---
+++ pertemuan_keenam/pertemuan_keenam.cpp
@@ -0,0 +1,20 @@
+// pertemuan_keenam.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int bilangan1, bilangan2;
+void inputData()
+{
+    cout << "Masukan bilangan pertama : ";
+    cin >> bilangan1;
+    cout << "Masukan bilangan kedua : ";
+    cin >> bilangan2;
+}
+
+int penjumlahan(int a, int b)
+{
+    return a + b;
+}
+
